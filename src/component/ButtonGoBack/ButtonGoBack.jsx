a7244905924b30d52c55422865c1b156ea812b87
IVKrylova <ivk.lis@yandex.ru>
2023-04-08 01:05:59 +0300

    feat: add html code of the personal page

---
+++ src/component/ButtonGoBack/ButtonGoBack.jsx
@@ -0,0 +1,13 @@
+import './ButtonGoBack.scss';
+
+const ButtonGoBack = (props) => {
+  return (
+    <button
+      className='button-go-back'
+      type='button'
+      aria-label='Кнопка вернуться назад'
+    ></button>
+  );
+}
+
+export default ButtonGoBack;
--- src/component/PersonalPage/PersonalPage.jsx
+++ src/component/PersonalPage/PersonalPage.jsx
@@ -1,10 +1,77 @@
+import ButtonExit from '../ButtonExit/ButtonExit';
+import ButtonGoBack from '../ButtonGoBack/ButtonGoBack';
 import './PersonalPage.scss';
 
 const PersonalPage = (props) => {
   return (
-    <main className='personal-page'>
-      {props.currentPerson.id}
-    </main>
+    props.currentPerson &&
+      <main className='personal-page'>
+        <section className='personal-page'>
+          <ul className='personal-page__button-list'>
+            <li>
+              <ButtonGoBack />
+            </li>
+            <li>
+              <ButtonExit />
+            </li>
+          </ul>
+          <h1 className='personal-page__name'>
+            {`${props.currentPerson.first_name} ${props.currentPerson.last_name}`}
+          </h1>
+          <p className='personal-page__position'>
+            Партнер
+          </p>
+          <img
+            src={props.currentPerson.avatar}
+            alt={`${props.currentPerson.first_name} ${props.currentPerson.last_name}`}
+            className='personal-page__img'
+          />
+        </section>
+        <section className='personal-page__content'>
+          <ul className='personal-page__contacts-list'>
+            <li>
+              <a
+                className='personal-page__link personal-page__link_phone'
+                href='tel:+79543334455'
+              >
+                +7 (954) 333-44-55
+              </a>
+            </li>
+            <li>
+              <a
+                className='personal-page__link personal-page__link_email'
+                href={`mailto:${props.currentPerson.email}`}
+              >
+                {props.currentPerson.email}
+              </a>
+            </li>
+          </ul>
+          <div className='personal-page__description'>
+            <p className='personal-page__text'>
+              Клиенты видят в нем эксперта по вопросам разработки комплексных
+              решений финансовых продуктов, включая такие аспекты, как
+              организационная структура, процессы, аналитика и ИТ-компоненты.
+              Он помогает клиентам лучше понимать структуру рисков их бизнеса,
+              улучшать процессы за счет применения новейших технологий и увеличивать
+              продажи, используя самые современные аналитические инструменты.
+            </p>
+            <p className='personal-page__text'>
+              В работе с клиентами недостаточно просто решить конкретную проблему
+              или помочь справиться с трудностями. Не менее важно уделять внимание
+              обмену знаниями: "Один из самых позитивных моментов — это осознание
+              того, что ты помог клиенту перейти на совершенно новый уровень
+              компетентности, уверенность в том, что после окончания проекта у клиента
+              есть все необходимое, чтобы дальше развиваться самостоятельно".
+            </p>
+            <p className='personal-page__text'>
+              Помимо разнообразных проектов для клиентов финансового сектора, Сорин
+              ведет активную предпринимательскую деятельность. Он является совладельцем
+              сети клиник эстетической медицины в Швейцарии, предлагающей инновационный
+              подход к красоте, а также инвестором других бизнес-проектов.
+            </p>
+          </div>
+        </section>
+      </main>
   );
 }
 
